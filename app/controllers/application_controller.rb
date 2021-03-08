b561eb085ec19b010f1dd867a8b50871f5df0675
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-03-07 20:56:02 -0500

    commit tree c270ee2dbcddafc94500c88456f53ff4e2ba28df

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,46 +7,59 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  
+
   get '/' do
     erb :home
   end
+
+  
 
   get '/registrations/signup' do
 
     erb :'/registrations/signup'
   end
 
-  post '/registrations' do
+  post '/registrations/signup' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
+    @user.save 
     session[:user_id] = @user.id
-
-    redirect '/users/home'
-  end
-
-  get '/sessions/login' do
-
-    # the line of code below render the view page in app/views/sessions/login.erb
-    erb :'sessions/login'
-  end
-
-  post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/users/home'
-    end
-    redirect '/sessions/login'
-  end
-
-  get '/sessions/logout' do
-    session.clear
-    redirect '/'
+    
+     redirect '/users/home'
   end
 
   get '/users/home' do
 
-    @user = User.find(session[:user_id])
+      @user = User.find_by(id: session[:user_id])
     erb :'/users/home'
   end
+ 
+
+  get '/sessions/login' do
+
+  #   # the line of code below render the view page in app/views/sessions/login.erb
+     erb :'sessions/login'
+  end
+
+  post '/sessions/login' do
+    @user = User.find_by(email: params["email"], password: params["password"])
+    # binding.pry
+    session[:user_id] = @user.id 
+  #   # @user = User.find_by(email: params[:email], password: params[:password])
+  #   # # session[:user_id] = @user.id 
+  #   # if @user
+  #   #   session[:user_id] = @user.id
+        redirect '/users/home'
+  #   # else
+  #   #   redirect '/sessions/login'
+  #   # end
+  #   # 
+  end
+
+  get '/sessions/logout' do
+    session.clear
+     redirect '/'
+  end
+
+  
 end
